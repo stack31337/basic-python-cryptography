5f22952d0b9c38fd67b6e9f15e6a2b748dcfa006
Hashkatz <153441844+Hashkqtz@users.noreply.github.com>
2023-12-11 22:25:45 +0300

    Update DecryptAES.py

--- DecryptAES.py
+++ DecryptAES.py
@@ -4,11 +4,11 @@ from Crypto.Protocol.KDF import PBKDF2
 import sys
 
 # Sabit değerler (anahtar ve IV)
-iv = b'p2O#\x1c\xb4 \x85\xeez\x17\x90\x97\xc15\xd4'  # IV
-key = b'\x90\xa2\xec\xff\x11X\x16\x15\xca\xaf\xda\xee\xd4\xc2d\xa8dC\x02\xa0\xf2AwP\x9e_\xac\x14\xc8\xc3D\xb7' # Key
+iv = b''  # IV
+key = b'' # Key
 
 # Şifreli metin (encrypt.py'den alınacak)
-ciphertext = b'\x1d\xf6C\xed\xa8`<\xf4\xe3\x160\x8d\x12\x81\x1aq\xdf\xd1\xaei\xa6\xfe\xccB\xd4Xa\x06\xa7\x85e~'
+ciphertext = b''
 
 # AES şifreli objesi oluşturma (CBC)
 cipher = AES.new(key, AES.MODE_CBC, iv)
